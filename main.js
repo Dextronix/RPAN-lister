d574482702aa5b17e28b4529c2e99017025fac07
Dextronix <48369769+Dextronix@users.noreply.github.com>
2020-04-21 19:44:47 -0700

    Code overhaul
    Added sorting functionality, not yet implimented
    
    Changed the way JSON data is handled
    
    Mild CSS changes

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+var streams = [];
+
+String.prototype.TrimToLen = function(length) {
+    return this.length > length ? this.substring(0, length) + "..." : this;
+}
+
 $(document).keyup(function(e) {
     if (e.keyCode === 13) alert("Coded by StoneIncarnate!"); // enter
     if (e.keyCode === 27) refreshData(); // esc
@@ -21,41 +27,72 @@ function formatTime(time) {
 }
 
 function refreshData() {
-    searchStreams();
+    streams = [];
+    parseStreams();
 }
 
-function searchStreams() {
+function parseStreams() {
+    /* Instead of processing the entire object, I first pull out the most important parts */
     fetch('https://strapi.reddit.com/broadcasts')
         .then(data => data.json())
         .then(json => {
-
             $("#tableFLIP").empty();
-
             if (json.status == "success") {
-
                 $.each(json.data, function(index, item) {
-                    let sugar = item.post; // JavaScript Diabetes
+                    let sugar = item.post; // JavaScript Diabetes				
                     let streamlink = sugar.url;
-                    let streamname = sugar.title;
-                    let subreddit = sugar.subreddit.prefixedName;
+                    let title = sugar.title;
+                    let subreddit = sugar.subreddit.name;
                     let username = sugar.authorInfo.name;
                     let upvotes = item.upvotes;
                     let downvotes = item.downvotes;
-                    let timeup = item.broadcast_time;
+                    let timeon = item.broadcast_time;
                     let timeleft = item.estimated_remaining_time;
-                    //style="display: none;"
-                    var markup = `<tr class="result" ><td><a target="_blank" title="open stream" onclick="openTab('${streamlink}')">${streamname}</a></td><td>${subreddit}</td><td><a target="_blank" title="open userpage" onclick="openTab('https://reddit.com/u/${username}')">u/${username}</a></td><td>${upvotes}/${downvotes}</td><td>${formatTime(timeup)}</td><td>${formatTime(timeleft)}</td></tr>`;
 
-                    $("table tbody").append(markup);
+                    streams.push({
+                        "link": streamlink,
+                        "title": title,
+                        "subreddit": subreddit,
+                        "username": username,
+                        "upvotes": upvotes,
+                        "downvotes": downvotes,
+                        "timeon": timeon,
+                        "timeleft": timeleft
+                    });
 
                 });
-
+                //listStreams("timeon", true);
+                listStreams();
             } else {
                 $("table tbody").append("An error occured, try again");
+               // ERROR
             }
-
         })
+}
+
+function listStreams(sort = 'none', isAsc = true) {
+    /* I then sort them and display */
+    var order;
+
+    /* feed sorting function "asc" and "desc" */
+    order = 1 == isAsc ? "asc" : "desc";
+
+    if (sort !== 'none') {
+        obj = _.orderBy(streams, sort, order);
+        console.log("Ordering by " + sort + ", " + order);
+    } else {
+        obj = streams;
+        console.log("Not ordering");
+    }
+
+    $("#tableFLIP").empty();
+
+    $.each(obj, function(index, item) {
+        var markup = `<tr class="result" ><td><a target="_blank" title="open stream" onclick="openTab('${item.link}')">${item.title.TrimToLen(50)}</a></td><td>r/${item.subreddit}</td><td><a target="_blank" title="open userpage" onclick="openTab('https://reddit.com/u/${item.username}')">u/${item.username}</a></td><td>${item.upvotes}</td><td>${item.downvotes}</td><td>${formatTime(item.timeon)}</td><td>${formatTime(item.timeleft)}</td></tr>`;
+
+        $("table tbody").append(markup);
+    });
 
 }
 
-searchStreams()
+parseStreams()
