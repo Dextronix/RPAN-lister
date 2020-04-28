668a008ecbfe187b507d0d7836eb032008088d09
Dextronix <48369769+Dextronix@users.noreply.github.com>
2020-04-27 20:13:17 -0700

    Highlighting , We're in the BARN
    + ability to highlight user streams!
    + BARN localstorage API

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var streams = [];
+var barn = new Barn(localStorage);
 
-String.prototype.TrimToLen = function(length) {
+String.prototype.TrimToLen = function (length) {
     return this.length > length ? this.substring(0, length) + "..." : this;
 }
 
@@ -8,7 +9,7 @@ var sort = "";
 var isAsc = false;
 var sortindex = -1;
 
-$("tr th").dblclick(function() {
+$("tr th").dblclick(function () {
     if (($(this).attr("sortby") == sort) && (sortindex < 2)) {
         sortindex++;
         sort = $(this).attr("sortby");
@@ -17,28 +18,28 @@ $("tr th").dblclick(function() {
         sort = $(this).attr("sortby");
     }
 
-    $("tr th").each(function() {
+    $("tr th").each(function () {
         $(this).removeClass();
     });
 
     switch (sortindex) {
-        case 0:
-            $(this).addClass("down");
-			isAsc = true;
-            break;
-        case 1:
-            $(this).addClass("up");
-			isAsc = false;
-            break;
-		default: 
-			sort = "";
-			break;
+    case 0:
+        $(this).addClass("down");
+        isAsc = true;
+        break;
+    case 1:
+        $(this).addClass("up");
+        isAsc = false;
+        break;
+    default:
+        sort = "";
+        break;
     }
-	
-	refreshData();
+
+    refreshData();
 });
 
-$(document).keyup(function(e) {
+$(document).keyup(function (e) {
     if (e.keyCode === 13) alert("Coded by StoneIncarnate!"); // enter
     if (e.keyCode === 27) refreshData(); // esc
 });
@@ -66,13 +67,16 @@ function refreshData() {
 }
 
 function parseStreams() {
+
+    let UserHL = barn.smembers('highlitUsers') || [];
+
     /* Instead of processing the entire object, I first pull out the most important parts */
     fetch('https://strapi.reddit.com/broadcasts')
         .then(data => data.json())
         .then(json => {
             $("#tableFLIP").empty();
             if (json.status == "success") {
-                $.each(json.data, function(index, item) {
+                $.each(json.data, function (index, item) {
                     let sugar = item.post; // JavaScript Diabetes				
                     let streamlink = sugar.url;
                     let title = sugar.title;
@@ -95,17 +99,18 @@ function parseStreams() {
                         "timeon": timeon,
                         "timeleft": timeleft,
                         "contviews": contviews,
-                        "tempviews": tempviews
+                        "tempviews": tempviews,
+                        "highlight": UserHL.includes(username.toLowerCase())
                     });
 
-                });			
-	
-				if (sort !== "") {
-					listStreams(sort, isAsc);
-				} else {
-					listStreams();
-				}
-						
+                });
+
+                if (sort !== "") {
+                    listStreams(sort, isAsc);
+                } else {
+                    listStreams();
+                }
+
             } else {
                 $("table tbody").append("An error occured, try again");
             }
@@ -113,52 +118,59 @@ function parseStreams() {
 }
 
 function listStreams(sort = 'none', isAsc = true) {
+
     /* I then sort them and display */
     var order;
 
+    /* let highlight = _.filter(streams, ["highlight", true]);  */
+    let refined = _.reject(streams, data => _.includes(barn.smembers('hiddenUsers'), data.username)); /* highlit and hidden are removed */
+
     /* feed sorting function "asc" and "desc" */
     order = 1 == isAsc ? "asc" : "desc";
+    master_index = 0;
 
     if (sort !== 'none') {
-        obj = _.orderBy(streams, sort, order);
+        obj = _.orderBy(refined, sort, order);
         console.log("Ordering by " + sort + ", " + order);
     } else {
-        obj = streams;
+        obj = refined;
         console.log("Not ordering");
     }
 
     $("#tableFLIP").empty();
 
-    $.each(obj, function(index, item) {
-        var markup = `<tr class="result" ><td>${index+1}</td><td data_value="streamIDhere" data_menu="stream"><a title="${item.title}"  onclick="openTab('${item.link}');">${item.title.TrimToLen(50)}</a></td><td data_value ="${item.subreddit}" data_menu="sub">r/${item.subreddit}</td><td data_value = "${item.username}" data_menu="user"><a onclick="openTab('https://www.reddit.com/user/${item.username}');">u/${item.username}</a></td><td>${item.contviews}</td><td>${item.upvotes}</td><td>${item.downvotes}</td><td>${formatTime(item.timeon)}</td><td>${formatTime(item.timeleft)}</td></tr>`;
+    $.each(obj, function (index, item) {
+
+        var markup = `<tr class="result" ${item.highlight?'style="background: lightyellow"':""}><td>${index+1}</td><td data_value="streamIDhere" data_menu="stream"><a title="${item.title}"  onclick="openTab('${item.link}');">${item.title.TrimToLen(50)}</a></td><td data_value ="${item.subreddit}" data_menu="sub">r/${item.subreddit}</td><td data_value = "${item.username}" data_menu="${item.highlight?"user2":"user"}"><a onclick="openTab('https://www.reddit.com/user/${item.username}');">u/${item.username}</a></td><td>${item.contviews}</td><td>${item.upvotes}</td><td>${item.downvotes}</td><td>${formatTime(item.timeon)}</td><td>${formatTime(item.timeleft)}</td></tr>`;
 
         $("table tbody").append(markup);
     });
 
-/*   
-	
-	This code will go live next update! 
-
-  $("tr td").bind("contextmenu", function(event) {
+    $("tr td").bind("contextmenu", function (event) {
         event.preventDefault();
         var data = $(this).attr("data_value");
 
         switch ($(this).attr("data_menu")) {
 
-            case "stream":
-                $(".custom-menu").html('<li data-action="stream_ID">Copy streamID</li>');
-                break;
+       /*  case "stream":
+            $(".custom-menu").html('<li data-action="stream_ID">Copy streamID</li>');
+            break; */
+		/* <li data-action="user_hide">Hide user</li> */
 
-            case "user":
-                $(".custom-menu").html('<li data-action="user_hlt">Highlight user</li><li data-action="user_hide">Hide user</li>');
-                break;
+        case "user":
+            $(".custom-menu").html('<li data-action="user_hlt">Highlight user</li>');
+            break; 
 
-            case "sub":
+        case "user2":
+            $(".custom-menu").html('<li data-action="user_unhlt">Unhighlight user</li>');
+            break;
+
+            /* case "sub":
                 $(".custom-menu").html('<li data-action="sub_hlt">Highlight sub</li><li data-action="sub_hide">Hide sub</li>');
-                break;
+                break; */
 
-            default:
-                $(".custom-menu").html('');
+        default:
+            $(".custom-menu").html('');
         }
 
         $(".custom-menu").finish().toggle(100).
@@ -167,34 +179,39 @@ function listStreams(sort = 'none', isAsc = true) {
             left: event.pageX + "px"
         });
 
-        $(document).bind("mousedown", function(e) {
+        $(document).bind("mousedown", function (e) {
             if (!$(e.target).parents(".custom-menu").length > 0) {
                 $(".custom-menu").hide(100);
             }
         });
 
-        $(".custom-menu li").click(function() {
+        $(".custom-menu li").click(function () {
             switch ($(this).attr("data-action")) {
-                case "stream_ID":
-                    alert("Copied strean ID: " + data);
-                    break;
-                case "user_hide":
-                    alert("Hiding user " + data);
-                    break;
-                case "user_hlt":
-                    alert("Hightling user " + data);
-                    break;
-                case "sub_hlt":
-                    alert("Hightling sub " + data);
-                    break;
-                case "sub_hide":
-                    alert("Hiding sub " + data);
-                    break;
+            case "user_hlt":
+                barn.sadd('highlitUsers', data);
+                refreshData(); //soft refresh
+                break;
+            case "user_unhlt":
+                barn.srem('highlitUsers', data);
+                refreshData(); //soft refresh
+                break;
+          /*case "stream_ID":
+                alert("Copied stream ID: " + data);
+                break;
+            case "user_hide":
+                alert("Hiding user " + data);
+                break; 
+            case "sub_hlt":
+                alert("Hightling sub " + data);
+                break;
+            case "sub_hide":
+                alert("Hiding sub " + data);
+                break; */
             }
             $(".custom-menu").hide(100);
         });
 
-    }); */
+    });
 
 }
 
