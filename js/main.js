f85dd221acd671cdca959dc89756717b6dce40d8
Dextronix <48369769+Dextronix@users.noreply.github.com>
2020-06-09 22:14:51 -0700

    Fix for "go back" in settings
    + unminified scripts

---
+++ js/main.js
@@ -0,0 +1,280 @@
+var streams = [];
+var barn = new Barn(localStorage);
+checkdarkmode();
+checkar();
+barn.condense();
+
+function checkdarkmode(){
+$("html").toggleClass("darkmode", barn.get("setting_darkmode") || false);
+}
+
+function checkar(){
+var ar = barn.get('setting_refresh') || false;
+var interval = barn.get('setting_interval') || 10;
+if (ar){
+var refreshloop = setInterval(function(){ refreshData(); }, interval*1000);
+}
+}
+
+$(document).ready(function () {
+    $.ajaxSetup({
+        cache: false
+    });
+});
+
+String.prototype.TrimToLen = function (length) {
+    return this.length > length ? this.substring(0, length) + "..." : this;
+}
+
+var BSmode = false || barn.get('setting_BSmode');
+var sort = "" || barn.get('sort_sort');
+var isAsc = false || barn.get('sort_isAsc');
+var sortindex = -1;
+var Elemindex = 0 || barn.get('sort_Elemindex');
+
+BSmode&&$("#resulttable thead tr").html("<th>#</th><th sortby=\"title\"><a>Stream</a></th><th sortby=\"subreddit\"><a>Subreddit</a></th><th sortby=\"username\"><a>Username</a></th>\t<th sortby=\"contviews\"><a>Viewers</a></th><th sortby=\"upvotes\"><a>Upvotes</a></th><th sortby=\"downvotes\"><a>Downvotes</a></th><th sortby=\"comments\"><a>Comments</a></th><th sortby=\"timeon\"><a>On for</a></th><th sortby=\"timeleft\"><a>Time Left</a></th></tr>");
+
+if (sort !== "" && Elemindex > 0) {
+    let elm = $("tr th");
+	if (isAsc){
+	elm.eq(Elemindex).addClass("down");
+        sortindex = 0;	
+	}
+	else{
+		elm.eq(Elemindex).addClass("up");
+        sortindex = 1;
+	}
+}
+
+$("tr th").dblclick(function () {
+
+    if (($(this).attr("sortby") == sort) && (sortindex < 2)) {
+        sortindex++;
+        sort = $(this).attr("sortby");
+    } else {
+        sortindex = 0;
+        sort = $(this).attr("sortby");
+    }
+
+    $("tr th").each(function () {
+        $(this).removeClass();
+    });
+
+
+	if (sortindex == 0){
+		$(this).addClass("down");
+        isAsc = true;
+	}
+    else if (sortindex == 1){
+		 $(this).addClass("up");
+        isAsc = false;
+	}
+	else {
+		sort = "";
+	}
+
+    barn.set('sort_Elemindex', $(this).index());
+    barn.set('sort_isAsc', isAsc);
+    barn.set('sort_sort', sort);
+    s_refreshData();
+	
+	});
+	
+
+window.addEventListener('keydown', function (e) {
+	if (e.key === "j"){
+		BSmode=!BSmode,barn.set("setting_BSmode",BSmode),BSmode?(alert("BSmode activated\nRefreshing..."),location.reload()):(alert("BSmode deactivated\nRefreshing..."),barn.set('sort_sort', ""),location.reload());
+	}
+    if (e.keyCode === 13) alert("Coded by StoneIncarnate!"); // enter
+    if (e.keyCode === 27) refreshData(); // esc	
+    if ((e.ctrlKey || e.metaKey) && e.keyCode == 88) console.log(`DEBUG VALUES:\nHighlighted users: ${barn.smembers('highlitUsers')}\nHidden users: ${(barn.smembers('hiddenUsers') || "none")}\nSort: ${barn.get('sort_sort')}\nisAsc: ${barn.get('sort_isAsc')}\nElemindex: ${barn.get('sort_Elemindex')}`);
+});
+
+function getList(index) {
+	if (index == 0){
+		return barn.smembers('highlitUsers') || [];
+	}
+	else if (index == 1){
+		return barn.smembers('hiddenUsers') || [];
+	}
+	else if (index == 2){
+		return barn.smembers('hiddenSubs') || [];
+	}
+	else if (index == 3){
+		return JSON.parse(barn.get("setting_trash"));
+	}
+}
+
+function openTab(src) {
+    window.open(src, "_blank");
+}
+
+function formatTime(time) {
+    var hrs = ~~(time / 3600);
+    var mins = ~~((time % 3600) / 60);
+    var secs = ~~time % 60; // hehe secs
+    var ret = "";
+    if (hrs > 0) {
+        ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+    }
+    ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+    ret += "" + secs;
+    return ret;
+}
+
+function refreshData() {
+    streams = [];
+    parseStreams();
+}
+
+function s_refreshData() {
+    listStreams(sort, isAsc);
+}
+
+function parseStreams() {
+    $("#tableFLIP").empty();
+    $("table tbody").append("Loading stream data...");
+
+    let UserHL = barn.smembers('highlitUsers') || [];
+
+    $.getJSON('https://strapi.reddit.com/broadcasts').done(function (json) {
+        $("#tableFLIP").empty();
+        $.each(json.data, function (index, item) {	
+            let sugar = item.post; // JavaScript Diabetes	
+			let comments = sugar.commentCount || "0";			
+            let streamlink = sugar.url || "error";
+            let title = sugar.title || "error";
+            let subreddit = sugar.subreddit.name || "error";
+            let username = sugar.authorInfo.name || "error";
+            let upvotes = item.upvotes || "0";
+            let downvotes = item.downvotes || "0";
+            let timeon = item.broadcast_time || "0";
+            let timeleft = item.estimated_remaining_time || "0";
+            let contviews = item.continuous_watchers || "0";
+            let tempviews = item.unique_watchers || "0";
+			let state = item.stream.state || "ENDED";
+
+            streams.push({
+				"comments": comments,
+				"state": state,
+                "link": streamlink,
+                "title": title.toLowerCase(),
+                "subreddit": subreddit.toLowerCase(),
+                "username": username.toLowerCase(),
+                "upvotes": upvotes,
+                "downvotes": downvotes,
+                "timeon": timeon,
+                "timeleft": timeleft,
+                "contviews": contviews,
+                /* "tempviews": tempviews, */
+                "highlight": UserHL.includes(username.toLowerCase())
+            });
+
+        });
+
+        if (sort !== "") {
+            listStreams(sort, isAsc);
+        } else {
+            listStreams();
+        }
+    }).fail(function (jqxhr) {
+         if (jqxhr.status == 200) {
+            alert("The RPAN server appears to have been blocked by something!\nYou may need to whitelist the RPAN stream API for this site!");
+        } else if (jqxhr.status == 500) {
+            $("#tableFLIP").empty();
+            $("table tbody").append(`  Error ${jqxhr.status} Refreshing...`);
+            _.delay(function() {
+                refreshData()
+            }, 500, 'later');
+        } else {
+            $("#tableFLIP").empty();
+            $("table tbody").append(`Error ${jqxhr.status}`);
+        }
+    });
+}
+
+function listStreams(sort = 'none', isAsc = true) {
+    var order;
+	let onlylive = _.filter(streams, data => _.includes("IS_LIVE", data.state)); /* only live streams */
+    let hideusers = _.reject(onlylive, data => _.includes(barn.smembers('hiddenUsers'), data.username)); /* hidden users are removed */
+	let hidesubs = _.reject(hideusers, data => _.includes(barn.smembers('hiddenSubs'), data.subreddit)); /* hidden subs are removed */
+	
+    order = 1 == isAsc ? "asc" : "desc";
+    master_index = 0;
+
+    if (sort !== 'none') {
+        obj = _.orderBy(hidesubs, sort, order);
+        console.log("Ordering by " + sort + ", " + order);
+    } else {
+        obj = hidesubs;
+        console.log("Not ordering");
+    }
+
+    $("#tableFLIP").empty();
+
+    $.each(obj, function (index, item) {		
+        var markup = "";
+		if (BSmode) {	
+		markup = `<tr class="result${item.highlight?' marked"':""}"><td>${index+1}</td><td data_value="streamIDhere" data_menu="stream"><a title="${item.title}"  onclick="openTab('${item.link}');">${item.title.TrimToLen(50)}</a></td><td data_value ="${item.subreddit}" data_menu="sub">r/${item.subreddit}</td><td data_value = "${item.username}" data_menu="${item.highlight?"user2":"user"}"><a onclick="openTab('https://www.reddit.com/user/${item.username}');">u/${item.username}</a></td><td>${item.contviews}</td><td>${item.upvotes}</td><td>${item.downvotes}</td><td sortby="comments">${item.comments}</td><td>${formatTime(item.timeon)}</td><td>${formatTime(item.timeleft)}</td></tr>`;
+		}
+		else{
+		markup = `<tr class="result${item.highlight?' marked"':""}"><td>${index+1}</td><td data_value="" data_menu="stream"><a title="${item.title}"  onclick="openTab('${item.link}');">${item.title.TrimToLen(50)}</a></td><td data_value ="${item.subreddit}" data_menu="sub">r/${item.subreddit}</td><td data_value = "${item.username}" data_menu="${item.highlight?"user2":"user"}"><a onclick="openTab('https://www.reddit.com/user/${item.username}');">u/${item.username}</a></td><td>${item.contviews}</td><td>${item.upvotes}</td><td>${item.downvotes}</td><td>${formatTime(item.timeon)}</td><td>${formatTime(item.timeleft)}</td></tr>`;
+		}
+        $("table tbody").append(markup);
+    });
+
+    $("tr td").bind("contextmenu", function (event) {
+        event.preventDefault();
+        var data = $(this).attr("data_value");
+		var menu = $(this).attr("data_menu");
+		
+		if (menu == "user"){
+			$(".custom-menu").html('<li data-action="user_hlt">Highlight user</li><li data-action="user_hide">Hide user</li>');
+		}
+		else if (menu == "user2"){
+			$(".custom-menu").html('<li data-action="user_unhlt">Unhighlight user</li><li data-action="user_hide">Hide user</li>');
+		}
+		else if (menu == "sub"){
+			$(".custom-menu").html('<li data-action="sub_hide">Hide Subreddit</li>');
+		}
+		else {
+			$(".custom-menu").html('');
+		}
+
+        $(".custom-menu").finish().toggle(100).
+        css({
+            top: event.pageY + "px",
+            left: event.pageX + "px"
+        });
+
+        $(document).bind("mousedown", function (e) {
+            if (!$(e.target).parents(".custom-menu").length > 0) {
+                $(".custom-menu").hide(100);
+            }
+        });
+
+        $(".custom-menu li").click(function () {
+		var action = $(this).attr("data-action");	
+			if (action == "user_hlt"){
+				barn.sadd('highlitUsers', data);
+                refreshData(); 
+			}
+			else if (action == "user_unhlt"){
+				barn.srem('highlitUsers', data);
+                refreshData();
+			}
+			else if (action == "user_hide"){
+				barn.sadd('hiddenUsers', data);
+                refreshData();
+			}
+			else if (action == "sub_hide"){
+				barn.sadd('hiddenSubs', data);
+                refreshData();
+			}
+			
+            $(".custom-menu").hide(100);
+        });
+    });
+}
+
+parseStreams();
---
+++ js/settings.js
@@ -0,0 +1,399 @@
+var barn = new Barn(localStorage);
+	checkdarkmode();
+var trashStack = [];
+var activeStack = {"selectedtmp":[],"activeList":0,"list":"0"}
+
+function checkdarkmode(){
+$("html").toggleClass("darkmode", barn.get("setting_darkmode") || false);
+}
+
+function goBack() {
+window.location.href = "/RPAN-lister";
+}
+
+
+/* trash controls*/
+$(".trashcontrols a").on("click", function() {
+	   var index = $(this).index();
+       if (trashStack.length > 0){
+		   if (index == 0){
+			   doRestore();
+		   }
+		   else if (index == 1){
+			   deletetrash();
+		   }
+	   }
+    });
+
+$(document).ready(function() {
+	$(".addbutton").on("click", function() {
+		var name = $(".namebox").val();
+		var regexpassed = true;
+       if (name.length > 0 && name.length < 20 && regexpassed){
+		   // owo fix this
+		   barn.sadd(getlistStr(activeStack.activeList), name)
+		   populateList(getList(activeStack.activeList));
+	   }
+	   else if (name.length > 20){
+		alert("Reddit user/sub names have a 20 character limit");   
+	   }
+	   else{
+		 alert("Invalid user/sub name");  
+	   }
+    });
+	
+	registerSettings();
+    registerSlider();
+    populateList(getList(0));
+});
+
+$(document).bind("mousedown", function(e) {
+    if (!$(e.target).parents(".custom-menu2").length > 0) {
+        $(".custom-menu2").hide(100);
+    }
+});
+
+/* Sets the active tab and active list which informs nearly every function 
+which list is currently being manipulated */
+$(".tabhead").on("click", function() {
+    $(".custom-menu2").hide();
+    activeStack.selectedtmp = [];
+    var action = $(this).index();
+    $(".tabhead").removeClass("active");
+    $(this).addClass("active");
+	
+	if (action == 0){
+		activeStack.activeList = 0;
+        populateList(getList(0));
+	}
+	else if(action == 1){
+activeStack.activeList = 1;
+            populateList(getList(1));
+	}
+	else if(action == 2){
+	activeStack.activeList = 2;
+            populateList(getList(2));	
+	}
+	else if(action == 3){
+		activeStack.state = "trash";
+			activeStack.activeList = 3;
+			populateTrash();
+	}
+});
+
+
+/* function manageBackup(create = true){
+var masterArray = {};
+
+masterArray = {"sort_sort":barn.get("sort_sort"),"sort_isAsc":barn.get("sort_isAsc"),"sort_Elemindex":barn.get("sort_Elemindex"),"setting_darkmode":barn.get("setting_darkmode"),"setting_BSmode":barn.get("setting_BSmode"),"setting_interval":barn.get("setting_interval"),"setting_refresh":barn.get("setting_refresh"),"setting_trash":barn.get("setting_trash"),"highlitUsers": barn.smembers("highlitUsers"),"hiddenUsers": barn.smembers("hiddenUsers"),"hiddenSubs": barn.smembers("hiddenSubs")};
+
+var compressed = LZString.compress(JSON.stringify(masterArray));
+
+}
+
+manageBackup() */
+
+/* returns barn list of index*/
+function getList(index) {
+	if (index == 0){
+		return barn.smembers('highlitUsers') || [];
+	}
+	else if (index == 1){
+		return barn.smembers('hiddenUsers') || [];
+	}
+	else if (index == 2){
+		return barn.smembers('hiddenSubs') || [];
+	}
+	else if (index == 3){
+		return JSON.parse(barn.get("setting_trash"));
+	}
+}
+
+/* This function diff checks two given arrays, finds the difference
+between the two and then adds or removes that difference to the barn list */
+
+function manageBarn(name, after){
+var before = barn.smembers(name);
+before = _.compact(before)
+after = _.compact(after);
+
+if (before.length > after.length){
+var newList = _.difference(before,after);
+$.each(newList, function( index, value ) {
+  barn.srem(name,value);
+});
+}
+else if (before.length < after.length) {
+	var newList = _.difference(after,before);
+	$.each(newList, function( index, value ) {
+  barn.sadd(name,value);
+}); 
+}
+barn.condense(); 
+}
+
+/* Sets a barn list using the above function*/
+function setList(index, value) {
+	if (index == 0){
+		manageBarn("highlitUsers",value);
+	}
+	else if (index == 1){
+	manageBarn("hiddenUsers",value);	
+	}
+	else if (index == 2){
+	manageBarn("hiddenSubs",value);	
+	}
+	else if (index == 3){
+	barn.set("setting_trash",JSON.stringify(value));	
+	}
+}
+
+/* returns the barn variable name of a list index*/
+function getlistStr(index) {
+	if (index == 0){
+	return "highlitUsers";
+	}
+	else if (index == 1){
+	return "hiddenUsers";	
+	}
+	else if (index == 2){
+	return "hiddenSubs";	
+	}
+}
+
+/* if highlit return hidden, vice versa*/
+function getMove() {
+   if (activeStack.activeList == 0) {
+	   return 1;
+   }
+   else if (activeStack.activeList == 1){
+	   return 0;
+   }
+   else
+   {
+	  return -1; 
+   }
+}
+
+$('.controls a').on("click", function() {
+    var action = $(this).index();
+	if (action == 0){
+		if (!_.isEmpty(activeStack.selectedtmp)) {
+				var move = getMove();
+                if (move == 1) {
+                    moveMenu("Hidden Users");
+                } else if (move == 0) {
+                    moveMenu("Highlit Users");
+                }
+				else {
+					moveMenu();
+				}					
+            }
+	}
+	else if(action == 1){
+	if (!_.isEmpty(activeStack.selectedtmp)) {
+                deleteMenu();
+            }	
+	}
+	else if(action == 2){
+	 $(".boxcontainer").toggle();
+	 $(".namebox").val("");	
+	}
+	
+	
+});
+
+/* Removes items from trash variable and adds them back to their original list*/
+function doRestore(){
+ $.each( trashStack, function(key, value) {
+	barn.sadd(getlistStr(value.list),value.name);
+});	
+deletetrash();
+}
+
+/* Copies selected items from one list to another */
+function doMove(newList) {
+	var mergedList = _.concat(getList(newList), activeStack.selectedtmp);	
+    setList(newList, mergedList);
+}
+
+/* Displays items in a defined list index*/
+function populateList(list) {
+	$(".boxcontainer").hide();
+	$(".namebox").val("");
+	
+	$(".controls").show();
+	$(".trashcontrols").hide();
+	
+    $(".listcontent").empty();
+	
+    $.each(list, function(index, value) {
+        var checkbox = '<input type="checkbox" class="chkbox" value="' + value + '" name="' + value + '"><label for="' + value + '">' + value + '</label><br>';
+        $(".listcontent").append($(checkbox));
+    })
+
+    $('.chkbox').on("click", function() {
+        activeStack.selectedtmp = [];
+        $("input[class='chkbox']:checked").each(function() {
+            activeStack.selectedtmp.push($(this).attr("name"));
+        });
+
+    });
+};
+
+/* this function removes selected items for the activeList and adds them to the trashStack if needed*/
+function doDelete(toTrash = true) {
+    var newList = _.difference(getList(activeStack.activeList), activeStack.selectedtmp);
+    setList(activeStack.activeList, newList);
+	if (toTrash){
+		itemizeTrash();
+	} 
+}
+
+/* Add each item in selection to trashstack preserving which list they came from */
+function itemizeTrash(){
+var i,TR = [];
+for (i = 0; i < activeStack.selectedtmp.length; i++) {
+  TR.push({"name":activeStack.selectedtmp[i],"list":activeStack.activeList}); // fix this
+} 
+
+trash = getList(3);
+trash = _.uniq(_.concat(TR,trash), _.isEqual);
+setList(3, trash);
+}
+
+/* Read the trash variable and display*/
+function populateTrash() {
+	$(".controls").hide();
+	$(".trashcontrols").show();
+    $(".listcontent").empty();
+	TR = _.compact(getList(3));;
+	
+    $.each(TR, function(key, value) {
+  var code = `<input type="checkbox" class="chkbox" data-list="${value.list}" data-name="${value.name}"><label for="${value.name}">${value.name}</label><br>`;
+  $(".listcontent").append($(code));
+});
+
+     $('.chkbox').on("click", function() {
+        trashStack = [];
+        $("input[class='chkbox']:checked").each(function() {
+            trashStack.push({"name":$(this).attr("data-name"),"list":_.toSafeInteger($(this).attr("data-list"))});
+        });
+    }); 
+			
+};
+
+/* Removes selected items from trash variable*/
+function deletetrash(){
+var newList = _.differenceWith(getList(3) ,trashStack, _.isEqual);	
+setList(activeStack.activeList, newList);
+populateTrash();
+}
+
+function moveMenu(string) {
+	if (string){
+    $(".custom-menu2").html('<li data-action="move">' + string + '</li><li data-action="trash">Trash</li>');
+	}
+	else
+	{
+	$(".custom-menu2").html('<li data-action="trash">Trash</li>');	
+	}
+    var p = $(".controls a").first();
+    var position = p.position();
+    $(".custom-menu2").finish().toggle(100).
+    css({
+        top: position.top + 5 + "px",
+        left: (position.left - 10) + "px"
+    });
+    registerMenu();
+}
+
+function deleteMenu() {
+    $(".custom-menu2").html('<li data-action="supertrash">Forever</li><li data-action="trash">To Trash</li>');
+    var p = $(".controls a").first();
+    var position = p.position();
+    $(".custom-menu2").finish().toggle(100).
+    css({
+        top: position.top + 5 + "px",
+        left: (position.left + 50) + "px"
+    });
+    registerMenu();
+}
+
+/* This function is called because after menu html is destroyed the events are removed
+here we reset that click event*/
+function registerMenu() {
+    $(".custom-menu2 li").on("click", function() {
+		var action = $(this).attr("data-action");	
+		if (action == "move"){
+			doMove(getMove());
+				doDelete(false);
+				populateList(getList(activeStack.activeList));
+		}
+		else if (action == "trash"){
+		doDelete(true);
+				populateList(getList(activeStack.activeList));	
+		}
+		else if (action == "supertrash"){
+		doDelete(false);
+			    populateList(getList(activeStack.activeList));	
+		}	
+        $(".custom-menu2").hide(100);
+    });
+}
+
+ function registerSettings(){
+var darkmode = barn.get("setting_darkmode") || false,
+ autorefresh = barn.get("setting_refresh") || false;
+ 
+$('[data-action=darkmode]').prop("checked",darkmode);
+$('[data-action=autorefresh]').prop("checked",autorefresh);
+
+ $("input[class='setting']").on("change", function() {
+	 var action = $(this).attr("data-action");	
+		if (action == "darkmode"){
+			darkmode = !darkmode;
+			   barn.set("setting_darkmode",darkmode);
+			   checkdarkmode();
+		}
+		else if (action == "autorefresh"){
+			autorefresh = !autorefresh;
+			   barn.set("setting_refresh",autorefresh); 
+			   registerSlider();
+			   	if (!autorefresh){
+				  barn.set("setting_interval", 10);	  
+				}
+		}
+		
+			$(".container").toggle(autorefresh);
+ });
+ $(".container").toggle(autorefresh);
+ barn.condense();
+} 
+
+/* this function handles the managing of misc settings and the interval slider */
+function registerSlider (){
+  var interval = barn.get("setting_interval") || 10;
+
+   $('.range-slider').empty();
+   $('.range-slider').append(`<input class="range-slider__range" type="range" value="${interval}" min="5" max="60"><span class="range-slider__value">0</span>`);
+   
+  var slider = $('.range-slider'),
+range = $('.range-slider__range'),
+value = $('.range-slider__value'); 
+   
+  slider.each(function(){	  
+    value.each(function(){
+	  var value = $(this).prev().attr('value');
+      $(this).html(interval + " s");
+    });	
+	
+    range.on('input', function(){
+      $(this).next(value).html(this.value  + " s"); 
+    });	
+	
+	range.on('mouseup touchend', function(){
+      barn.set("setting_interval",this.value)
+    });	
+  });  
+} 
